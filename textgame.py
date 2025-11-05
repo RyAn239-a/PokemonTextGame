2eff997c1be3e5246bf9409b0454aea6d535f6b5
Ryan <fanryan239@gmail.com>
2025-11-05 10:51:05 -0500

    Implement Pokémon leveling and experience gain
    Added experience and leveling system for starter Pokémon.

--- textgame.py
+++ textgame.py
@@ -1,5 +1,7 @@
-import random, time, sys
+# pokemon battle game
+import random, time, sys, math
 
+# print and input like a typewriter
 def typewriter(message, delay=0.02, end='\n'):
     for char in message:
         sys.stdout.write(char)
@@ -11,13 +13,12 @@ def typewriter(message, delay=0.02, end='\n'):
 def typewriterInput(prompt, delay=0.02):
     typewriter(prompt, delay, end='')
     return input()
-# pokemon battle game
 
-# define the pokemon 
+# define the pokemon
 starterPokemon = [
-    {"name": "Bulbasaur", "health": 45, "attack": 49, "defense": 49},
-    {"name": "Squirtle", "health": 44, "attack": 48, "defense": 65},
-    {"name": "Charmander", "health": 39, "attack": 52, "defense": 43}
+    {"name": "Bulbasaur", "health": 45, "attack": 49, "defense": 49, "exp": 0, "level": 1},
+    {"name": "Squirtle", "health": 44, "attack": 48, "defense": 65, "exp": 0, "level": 1},
+    {"name": "Charmander", "health": 39, "attack": 52, "defense": 43, "exp": 0, "level": 1}
 ]
 
 forestPokemon = [
@@ -43,9 +44,9 @@ typewriter("Get ready to begin your Pokemon Journey! \nBefore you begin you must
 typewriter(f"1. {starterPokemon[0]["name"]}\n2. {starterPokemon[1]["name"]}\n3. {starterPokemon[2]["name"]}")
 print()
 starter = starterPokemon[int(typewriterInput("Select 1, 2 or 3: ")) - 1]
-typewriter(f"You have selected {starter["name"]} as your starter pokemon")
+typewriter(f"You have selected {starter["name"]} as your starter pokemon.")
 
-#get random starting location and assign the wild pokemon accordingly to region 
+#get random starting location and assign the wild pokemon accordingly to region
 wildPokemon = []
 location = random.randint(0, 2)
 if location == 0:
@@ -57,24 +58,70 @@ elif location == 1:
 else:
     location = "desert"
     wildPokemon = desertPokemon
-typewriter(f"Your starting location is the {location} region")
+typewriter(f"Your starting location is the {location} region.")
 
 playerAlive = True
 while playerAlive:
+    # track leveling up
+    starterLevel = starter["level"]
+
+    # you encounter a wild pokemon
     encounter = wildPokemon[random.randint(0, 2)]
     print()
-    typewriter(f"You have encountered a {encounter["name"]}")
-    action = int(typewriterInput("Would you like to fight(1) or run away(2): "))
-    if action == 1:
-        damage = int(starter["attack"]/encounter["defense"] * (10) + random.randint(0, 10))
-        typewriter(f"You have dealt {damage} damage to {encounter["name"]}")
-        encounter["health"] = encounter["health"] - damage
-        if encounter["health"] >= 0: 
-            typewriter(f"{encounter["name"]} has died")
+    typewriter(f"You have encountered a {encounter["name"]}.")
+
+    # while it is alive, do these actions
+    encounterAlive = True
+    while encounterAlive:
+        action = int(typewriterInput("Would you like to fight(1) or run away(2): "))
+        print()
+
+        # attack action
+        if action == 1:
+            damage = int(starter["attack"]/encounter["defense"] * (10) + random.randint(0, 10))
+            typewriter(f"You have dealt {damage} damage to {encounter["name"]}")
+            encounter["health"] = encounter["health"] - damage
+            if encounter["health"] <= 0:
+                typewriter(f"{encounter["name"]} has died.")
+                print()
+
+                # level up the pokemon
+                starter["exp"] += 50
+                starter["level"] = int(100 * math.sqrt(starter["exp"]/1000))
+
+                # if pokemon leveled up
+                if starter["level"] > starterLevel:
+                    levelup = starter["level"] - starterLevel
+                    starter["health"] += 5 * levelup
+                    starter["attack"] += 5 * levelup
+                    starter["defense"] += 5 * levelup
+
+                encounterAlive = False
+                break
+            else:
+                typewriter(f"{encounter["name"]} has {encounter["health"]} health left.")
+                print()
+
+        # runaway action
+        elif action == 2:
+            runawayChance = random.randint(0, 2)
+            if runawayChance != 2:
+                typewriter("You successfully escaped battle.")
+                print()
+                encounterAlive = False
+                break
+            else:
+                typewriter("You failed to run away...")
+                print()
+
+        # wild pokemon attacks you
+        damage = int(encounter["attack"]/starter["defense"] * (10) + random.randint(0, 10))
+        typewriter(f"{encounter["name"]} dealt {damage} damage to your pokemon")
+        starter["health"] = starter["health"] - damage
+        if starter["health"] <= 0:
+            typewriter("Your pokemon has died.")
+            print()
+            quit()
         else:
-            typewriter(f"{encounter["name"]} has {encounter["health"]} health left")
-    elif action == 2:
-        continue
-    quit()
-
-
+            typewriter(f"Your pokemon has {starter["health"]} health left.")
+            print()
